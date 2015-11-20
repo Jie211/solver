3457a6a4e64764433cf9371b682df18e2bf8f5a3
chen@MP <jie211.jp@gmail.com>
2015-11-20 16:38:07 +0900

    fix

--- solvers.c
+++ solvers.c
@@ -3,23 +3,23 @@
 int SolverSelecter(double *val, int *col, int *ptr, double *bvec, double *xvec, int ndata, double eps, int i_max, int kskip, int fix){
   int error=0;
 
-if(S_CG){
-  error=CG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
-}else if(S_CR){
-  error=CR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
-}else if(K_CG){
-  error=KSKIPCG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, kskip, fix);
-}else if(K_CR){
-  error=KSKIPCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, kskip, fix);
-}else if(VP_CG){
-  error=VPCG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
-}else if(VP_CR){
-  error=VPCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
-}else if(VP_GCR){
-  error=VPGCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, restart_outer);
-}else if(S_GCR){
-  error=GCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, restart_outer);
-}
+  if(S_CG){
+    error=CG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
+  }else if(S_CR){
+    error=CR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
+  }else if(K_CG){
+    error=KSKIPCG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, kskip, fix);
+  }else if(K_CR){
+    error=KSKIPCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, kskip, fix);
+  }else if(VP_CG){
+    error=VPCG_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
+  }else if(VP_CR){
+    error=VPCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max);
+  }else if(VP_GCR){
+    error=VPGCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, restart_outer);
+  }else if(S_GCR){
+    error=GCR_CRS(val, col, ptr, bvec, xvec, ndata, eps, i_max, restart_outer);
+  }
 
   if(error==1){
     Display_Mes("good");
